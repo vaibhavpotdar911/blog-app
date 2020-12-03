28ae8afebd0a6498fe14df116389d56babdc5149
Vaibhav Potdar <vaibhav.potdar9946@gmail.com>
2020-12-03 23:13:39 +0530

    PostgreSQL connected as the database and migrated successfully

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    posted_date = models.DateTimeField(default=timezone.now())
+    posted_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoProject',
+        'USER': 'postgres',
+        'PASSWORD': 'vaibhav911',
+        'HOST': 'localhost',
     }
 }
 
