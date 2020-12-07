3d024bfe31fbfe77a192cd01dc47ea0b745e3b61
Vaibhav Potdar <vaibhav.potdar9946@gmail.com>
2020-12-07 21:32:35 +0530

    Registration page added

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
