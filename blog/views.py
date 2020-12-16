32077d718a9ed2e7dcf3b6b6129894fd96030bc3
Vaibhav Potdar <vaibhav.potdar9946@gmail.com>
2020-12-16 16:23:21 +0530

    Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 def home(request):
     context = {
@@ -9,12 +16,22 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-posted_date']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-posted_date')
 
 class PostDetailView(DetailView):
     model = Post
