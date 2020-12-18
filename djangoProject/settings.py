877c5b8293852177fddcd19a808f5f24c9f0bd90
Vaibhav Potdar <63528831+vaibhavpotdar911@users.noreply.github.com>
2020-12-18 14:30:01 +0530

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vaibhav.potdar9946@gmail.com'
-EMAIL_HOST_PASSWORD = 'eehgupddqppslntn'
+EMAIL_HOST_USER = os.environ.get('USER_NAME')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PASS')
